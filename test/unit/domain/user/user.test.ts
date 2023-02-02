b30577e557237023c6295e8953d8e041246800b6
lucasrluz <lucasr.luzbr@gmail.com>
2023-02-02 19:29:03 -0300

    test: adicionado testes para validação do nome do usuário

--- test/unit/domain/user/user.test.ts
+++ test/unit/domain/user/user.test.ts
@@ -17,4 +17,53 @@ describe('Tests do domínio usuário', () => {
       password: 'passwordtest',
     });
   });
+
+  it('Esprero que retorne um erro, dado um nome vazio', async () => {
+    const user = User.create('', 'usernametest', 'emailtest', 'passwordtest');
+
+    expect(user.isError()).toEqual(true);
+    expect(user.value).toEqual('O campo nome não pode estar vazio.');
+  });
+
+  it('Esprero que retorne um erro, dado um nome maior que 20 caracteres', async () => {
+    const user = User.create(
+      'nametestnametestnametest',
+      'usernametest',
+      'email@test.com',
+      'passwordtest',
+    );
+
+    expect(user.isError()).toEqual(true);
+    expect(user.value).toEqual(
+      'O campo nome tem um tamanho máximo de 20 caracteres.',
+    );
+  });
+
+  it('Esprero que retorne um erro, dado um nome com um valor numérico', async () => {
+    const user = User.create(
+      'nametest1',
+      'usernametest',
+      'email@test.com',
+      'passwordtest',
+    );
+
+    expect(user.isError()).toEqual(true);
+    expect(user.value).toEqual(
+      'O campo nome deve ter somente letras e pode conter espaços.',
+    );
+  });
+
+  it('Esprero que retorne um erro, dado um nome com caractere especial underline', async () => {
+    const user = User.create(
+      'name_test',
+      'usernametest',
+      'email@test.com',
+      'passwordtest',
+    );
+
+    expect(user.isError()).toEqual(true);
+    expect(user.value).toEqual(
+      'O campo nome deve ter somente letras e pode conter espaços.',
+    );
+  });
 });
